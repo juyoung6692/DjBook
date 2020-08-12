7edcfa478a9e8bee75a68a7f39b8eb7172565a06
juyoung <wanke0524@gmail.com>
2020-08-12 22:17:31 +0900

    blog Post list, Archive(Year, Month, Day) page, view구현

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,19 @@
 #blog/urls.py
-from django.urls import path
+from django.urls import path, re_path
+from blog import views
 
+app_name='blog'
 urlpatterns = [
+    path('',views.PostLV.as_view(), name='index'),
 
+    #same as /blog/
+    path('post/', views.PostLV.as_view(), name='post_list'),
+
+    #한글이 포함된 슬러그 처리, path('post/<slug:slug>로 하면 한글이 포함된 슬러그는 처리하지 못함
+    re_path(r'^post/(?P<slug>[-\w]+)/$', views.PostDV.as_view(), name='post_detail'),
+    path('archive/', views.PostAV.as_view(), name='post_archive'),
+    path('archive/<int:year>/', views.PostYAV.as_view(), name='post_year_archive'),
+    path('archive/<int:year>/<str:month>/', views.PostMAV.as_view(), name='post_month_archive'),
+    path('archive/<int:year>/<str:month>/<int:day>/', views.PostDAV.as_view(), name='post_day_archive'),
+    path('archive/today/', views.PostTAV.as_view(), name='post_today_archive'),
 ]
