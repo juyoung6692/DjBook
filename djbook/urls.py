5e1eef4b9ee53700d72dbebb69f16835fee00047
juyoung <wanke0524@gmail.com>
2020-08-15 17:30:54 +0900

    home html navbar틀

--- djbook/urls.py
+++ djbook/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from djbook.views import HomeView
 
 urlpatterns = [
+    path('/', HomeView.as_view(), name='home'),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
